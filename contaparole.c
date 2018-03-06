3c629dc8f2b9b2df5b3c1a23f4d5fcdda48674e5
CarloRuzzon <carloruzzon@gmail.com>
2018-03-06 11:38:37 +0100

    concluso con gli stati

--- contaparole.c
+++ contaparole.c
@@ -1,21 +1,43 @@
+/*stati:
+ * A = all'interno della parola
+ * B = all'esterno della parola
+ 
+  TABELLA
+
+  A -> B : QUANDO SIAMO ALL'ESTERNO
+  A -> A : ANCORA ALL'INTERNO
+  B -> B : DALL'ESTERNO ALL'ESTERNO
+  B -> A : DALL'ESTERNO ALL'INTERNO
+
+ */
 #include<stdio.h>
+
 
 int main()
 {
   int c, p = 0, cc = 0, r = 0; 
-
+  
+  enum STATO {A, B};
+  
+  enum STATO stato = B;
+  
   while ((c = getchar()) != EOF){
     
-    if (c == ' '){
-      p++;
-    }
+	  cc++;
+
+	  if (c == '\n') r++;
+	  
+	  if (stato == A){
+		  if (c == '\t' || c == '\n' || c == ' '){
+			  stato = B;
+		  }
+	  }
+	  
+	  else if(stato == B){
+		p++;
+		stato = A;
+	  }	
     
-    else if (c == '\n'){
-      r++;
-    }
-    else{
-      cc++;
-    }
   } 
   printf("Ci sono: %d caratteri, %d righe e %d parole.\n", cc, r, p);
   return 0;
