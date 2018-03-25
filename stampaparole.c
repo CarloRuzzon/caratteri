9ca10ccfb0a0aed0c979dd7af86576399240521e
CarloRuzzon <carloruzzon@gmail.com>
2018-03-25 12:05:45 +0200

    correzione

--- stampaparole.c
+++ stampaparole.c
@@ -3,10 +3,10 @@
  * IN -> ALL'INTERNO DELLA PAROLA
  * OUT -> ALL'ESTERNO DELLA PAROLA
  *
- * da IN a IN -> if c != ' ' or c != '/t' or c != '/n'
- * da IN a OUT -> if c == ' ' or c == '/t' or c == '/n'
- * da OUT a OUT -> impossibile
- * da OUT a IN -> if c!= ' ' or c != '/t' or c != '/n'
+ * Stato   carattere in ingresso                   carattere in uscita  nuovo stato
+ * in      c != ' ' && c!= '\t' && c != '\n'       c                    in
+ * in      c == ' ' or c == '\t' or c == '\n'      \n                   out
+ * OUT     c!= ' ' or c != '\t' or c != '\n'       c                    in
  */
 #include<stdio.h>
 
@@ -17,18 +17,21 @@ int main(){
 
 	while ((c = getchar()) != EOF){
 		if (stato == IN){
-                        if (c != ' ' && c!= '\t' && c!= '\n'){
-				putchar(c);
-			}
-			else{
-			       	stato = IN;
-				putchar('\n');
-			}
-			
-		}
-		else if (stato == OUT){
-			stato = IN;
-		}
-	}
-	return 0;
+      if (c != ' ' && c!= '\t' && c!= '\n'){
+        putchar(c);
+      }
+      else{
+        stato = OUT;
+        putchar('\n');
+      }
+    }
+    else if (stato == OUT){
+      stato = IN;
+      putchar(c);
+    }
+  }
+  return 0;
 }
+
+
+
